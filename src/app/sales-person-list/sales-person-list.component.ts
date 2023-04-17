38de415f2c792f3cbf42a4f2d726783c2a67beb1
VeeraEzhilVendhan <veeraezhilvendhan.v@gmail.com>
2023-04-17 10:04:13 +0530

    Table - using ngFor

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person'; 
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent {
+
+  salesPersonsList: SalesPerson[]=[
+    new SalesPerson("arun","a","arun@gmail.com","20"),
+    new SalesPerson("varun","v","varun@gmail.com","40"),
+    new SalesPerson("tharun","k","tharun@gmail.com","60"),
+    new SalesPerson("john","a","john@gmail.com","10"),
+  ]; 
+
+
+
+
+}
